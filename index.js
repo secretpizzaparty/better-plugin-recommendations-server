c6737e707a9a52ad2467086440dcd417f5a5561e
Joey Kudish <joey@jkudish.com>
2017-04-20 10:33:04 -0700

    redirect `/` route to github

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.route({
     method: 'GET',
     path:'/',
     handler: function (request, reply) {
-        return reply('Sup?');
+        return reply.redirect('https://github.com/secretpizzaparty/better-plugin-recommendations-server');
     }
 });
 
